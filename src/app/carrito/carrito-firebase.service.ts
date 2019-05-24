217e816c7aa97ff75dbd8f75252503fd71de0d2b
fernando mamani <ferchomamani2019@gmail.com>
2019-05-24 17:51:51 -0400

    ya puede agregar a carrito y spam carrito se incrementa aun en proceso..

--- src/app/carrito/carrito-firebase.service.ts
+++ src/app/carrito/carrito-firebase.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {Product} from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarritoFirebaseService {
-
-  constructor() { }
+    enCarrito: Product[] = [];
+ 
+   constructor(private db: AngularFirestore) {
+    
+  }
+  getProducts(): any{
+    return this.db.collection('products').valueChanges();
+  }
+  addProduct(p: Product): void {
+      this.enCarrito.push(p);
+  }
+  getProductOnCarrito(): Product[] {
+      return this.enCarrito;
+  }
 }
--- src/app/carrito/carrito.module.ts
+++ src/app/carrito/carrito.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarritoComponent } from './carrito.component';
 import { CarritoRoutingModule } from './carrito-routing.module';
+import {CarritoFirebaseService} from './carrito-firebase.service';
 
 @NgModule({
   declarations: [CarritoComponent],
